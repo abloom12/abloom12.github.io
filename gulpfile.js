d75a0b9f673f7d6ef87992a917ee904f68a7d8a0
Ash Bloomingdale <a.colton327@gmail.com>
2017-11-16 22:04:18 -0500

    …

--- gulpfile.js
+++ gulpfile.js
@@ -2,57 +2,38 @@
 const gulp = require('gulp');
 // require gulp devDependencies
 const $ = require('gulp-load-plugins')({
-    pattern: ['*'],
+    // pattern: ['*'],
     scope: ['devDependencies']
 });
+// require devDependencies
+const del = require('del');
+const browserSync = require('browser-sync').create();
 
-// init browser-sync server
+// start a browser-sync server
 gulp.task('browser-sync', () => {
   browserSync.init({
     server: './build',
     open: true
   });
 });
-// delete build folder
-gulp.task('clean', () => {
-	return $.del('build').then(paths => {
-    console.log(paths.join('\n'), 'Have Been Deleted Successfully');
-	});
+// compile scss
+gulp.task('css', () => {
+	return gulp.src('src/scss/styles.scss')
+      		.pipe($.plumber())
+      		.pipe($.sourcemaps.init())
+      		.pipe($.sass())
+      		.pipe($.autoprefixer({
+      			browsers: ['last 2 versions']
+      		}))
+      		.pipe(gulp.dest('assets/css'))
+      		.pipe($.cleanCss())
+      		.pipe($.rename({ suffix: '.min' }))
+      		.pipe($.sourcemaps.write('./'))
+      		.pipe(gulp.dest('assets/css'))
+          .pipe(browserSync.stream());
 });
-
-/* ---------- Development ---------- */
-
-// copy html to build
-gulp.task('html', () => {
-  return gulp.src('index.html', { base: 'src'})
-        .pipe(gulp.dest('build'));
-});
-// compile scss, autoprefix, beautify
-gulp.task('scss', () => {
-  return gulp.src('scss/styles.scss')
-          .pipe($.plumber())
-          .pipe($.sourcemaps.init())
-          .pipe($.sass())
-          .pipe($.autoprefixer())
-          .pipe($.sourcemaps.write('./'))
-          .pipe($.cssbeautify())
-          .pipe(gulp.dest('build/assets/css'))
-          .pipe($.browserSync.stream());
-});
-gulp.task('build', ['scss', 'html']);
 // watch for changes to src files, update browser
-gulp.task('watch', ['build', 'browser-sync'], () => {
-	gulp.watch('scss/**/*.scss', ['scss']);
-  gulp.watch('index.html', ['html']).on('change', $.browserSync.reload);
-});
-/* ---------- End Development ---------- */
-
-/* ---------- Production ---------- */
-
-// minify css
-gulp.task('css', ['scss'], () => {
-  return gulp.src('build/assets/styles.css')
-          .pipe($.plumber())
-          .pipe($.cleanCss())
-          .pipe(gulp.dest('assets/css'))
+gulp.task('watch', ['browser-sync'], () => {
+	gulp.watch('src/scss/**/*.scss', ['css']);
+  gulp.watch('index.html').on('change', browserSync.reload);
 });
