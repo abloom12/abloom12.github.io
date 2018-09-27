3404fa68816c4b9c2a580b8ee1d2416b873bed3c
Ash Bloomingdale <ash.bloomingdale@primarysolutions.net>
2018-09-27 08:40:52 -0400

    gulpfile and package.json update

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,26 @@
-// require gulp
 const gulp = require('gulp');
-// require gulp devDependencies
-const $ = require('gulp-load-plugins')({
-    // pattern: ['*'],
-    scope: ['devDependencies']
-});
-// require devDependencies
-const del = require('del');
-const browserSync = require('browser-sync').create();
 
-// start a browser-sync server
-gulp.task('browser-sync', () => {
-  browserSync.init({
-    server: './build',
-    open: true
-  });
+const $ = require('gulp-load-plugins')({
+  pattern: ['*'],
+  scope: ['devDependencies']
 });
-// compile scss
-gulp.task('css', () => {
-	return gulp.src('src/scss/styles.scss')
-      		.pipe($.plumber())
-      		.pipe($.sourcemaps.init())
-      		.pipe($.sass())
-      		.pipe($.autoprefixer({
-      			browsers: ['last 2 versions']
-      		}))
-      		.pipe(gulp.dest('assets/css'))
-      		.pipe($.cleanCss())
-      		.pipe($.rename({ suffix: '.min' }))
-      		.pipe($.sourcemaps.write('./'))
-      		.pipe(gulp.dest('assets/css'))
-          .pipe(browserSync.stream());
+
+//const bs = $.browsersync.create();
+
+gulp.task('scss', () => {
+  const plugins = [
+    $.autoprefixer(),
+    $.cssnano()
+  ];
+  return gulp.src('src/scss/styles.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass())
+    .pipe($.postcss(plugins))
+    .pipe($.sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/css'));
 });
-// watch for changes to src files, update browser
-gulp.task('watch', ['browser-sync'], () => {
-	gulp.watch('src/scss/**/*.scss', ['css']);
-  gulp.watch('index.html').on('change', browserSync.reload);
+
+gulp.task('watch', () => {
+  gulp.watch('src/scss/**/*.scss', ['scss']);
 });
