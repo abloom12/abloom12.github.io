3c92d34606219e2a045a81fa5dfe30016c6e5416
ash <a.bloomingdale327@gmail.com>
2018-10-16 09:47:52 -0400

    removed tmp files

--- anywhere src/session/Connections.js
+++
@@ -1,6 +0,0 @@
-$.webServer = {};
-$.webServer.protocol = "http";
-$.webServer.address = "localhost";
-$.webServer.port = "58598";
-$.webServer.serviceName = "./service/Anywhere.svc";
-$.webServer.anyerr = "http://anyerr.primarysolutions.net/Default.aspx";
